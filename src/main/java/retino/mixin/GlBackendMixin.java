27556a22b8a80ee3b701e0117c6b6e242298ad7b
Alvinn8 <42838560+Alvinn8@users.noreply.github.com>
2026-05-27 09:29:32 +0200

    Update to 26.1 (#10)

--- src/main/java/retino/mixin/GlBackendMixin.java
+++ src/main/java/retino/mixin/GlBackendMixin.java
@@ -1,20 +1,17 @@
 package retino.mixin;
 
-import net.minecraft.client.util.Window;
+import com.mojang.blaze3d.opengl.GlBackend;
 import org.lwjgl.glfw.GLFW;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
 
-@Mixin(value = Window.class)
-public abstract class WindowHintsMixin {
+@Mixin(value = GlBackend.class)
+public abstract class GlBackendMixin {
 	@Inject(
-		at = @At(
-			value = "INVOKE",
-			target = "Lorg/lwjgl/glfw/GLFW;glfwCreateWindow(IILjava/lang/CharSequence;JJ)J"
-		),
-		method = "<init>"
+		at = @At("HEAD"),
+		method = "setWindowHints"
 	)
 	private void adjustWindowHints(CallbackInfo callbackInfo) {
 		GLFW.glfwWindowHint(GLFW.GLFW_COCOA_RETINA_FRAMEBUFFER, GLFW.GLFW_FALSE);
